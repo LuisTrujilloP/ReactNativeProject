51ef932634b958afd027b8fba3c5cb229d53dd91
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2021-09-26 14:02:17 -0500

    Finalizando la calculadora de descuentos para una tienda

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 
 // const precio = 100;
 // const descuento = 15;
-function onClickButtonDescuentoPrecio()
-{
-  const Iprecio = document.getElementById("Inputprecio");
-  const precio = Iprecio.value;
-  const Idescuento = document.getElementById("Inputdescuento");
-  const precio = Idescuento.value;
-}
 
 function descuentoPrecio(precio, descuento)
 {
   const porcentajePrecioConDescuento = 100 - descuento;
-  const precioConDescuento = (precio * descuento)/100;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+  
   return precioConDescuento;
 
 }
+
+
+function onClickButtonDescuentoPrecio()
+{
+  const inputPrice = document.getElementById("InputPrice");
+  const Price = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const Discount = inputDiscount.value;
+
+
+  const precioConDescuento = descuentoPrecio(Price, Discount);
+  
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText ="el precio con descuento son " + precioConDescuento
+
+}
+
+
+
 // # Taller 3
 
 // -1: definir las formulas
