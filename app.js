e48993922da909a17123c6e115a8b3101acc905b
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2021-09-26 11:47:17 -0500

    Adiriendo a Html

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const precioOriginal = 100;
-const descuento = 15;
-const porcentajePrecioConDescuento = 100 - descueto;
 
+// const precio = 100;
+// const descuento = 15;
+function onClickButtonDescuentoPrecio()
+{
+  const Iprecio = document.getElementById("Inputprecio");
+  const precio = Iprecio.value;
+  const Idescuento = document.getElementById("Inputdescuento");
+  const precio = Idescuento.value;
+}
 
+function descuentoPrecio(precio, descuento)
+{
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * descuento)/100;
+  return precioConDescuento;
 
+}
 // # Taller 3
 
 // -1: definir las formulas
